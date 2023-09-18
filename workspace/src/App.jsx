4b20ece9b010332cdf1311d90ee52b8e90998828
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-09-18 15:59:47 +0200

    added Mainpage component

--- workspace/src/App.jsx
+++ workspace/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Body from './components/Body'
+import Mainpage from './components/Mainpage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Body />
+      <Mainpage />
     </>
   )
 }
---
+++ workspace/src/components/Mainpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Mainpage() {
+  return (
+    <>
+        <h2>Selected works</h2>
+    
+    </>
+  )
+}
+
+export default Mainpage
