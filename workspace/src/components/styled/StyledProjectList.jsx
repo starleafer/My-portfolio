56bb4e7f55108ddc448afe5c8ea607ebce60a150
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-09-18 17:40:03 +0200

    atempt on adding stylesheet

--- workspace/src/components/styled/StyledProjectList.jsx
+++ workspace/src/components/styled/StyledProjectList.jsx
@@ -3,7 +3,12 @@ import styled from 'styled-components'
 export const StyledProjectLIst = styled.div`
 .projectContainer {
     display: flex;
-    align-items: row;
+    color: white;
+    /* justify-content: center; */
+    flex-direction: column;
+    align-items: center;
     
+    /* align-items: row; */
+
 }
 `
