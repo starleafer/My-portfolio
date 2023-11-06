2f412330d89bc5c0a4a90d53447e591195cf7767
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-11-06 10:49:38 +0100

    added profile img

--- workspace/src/components/pages/AboutMe.jsx
+++ workspace/src/components/pages/AboutMe.jsx
@@ -16,17 +16,17 @@ function AboutMe() {
             seamless user experiences.
             <br />
             <br />
-            Proficient in JavaScript and specialized
-            in React, I thrive on the challenge of crafting dynamic and
-            interactive web applications. With a keen eye for design and
-            functionality, I believe in the power of technology to enhance the way we interact with the digital world. 
+            Proficient in JavaScript and specialized in React, I thrive on the
+            challenge of crafting dynamic and interactive web applications. With
+            a keen eye for design and functionality, I believe in the power of
+            technology to enhance the way we interact with the digital world.
             <br />
             <br />
-            My journey as a developer
-            has exposed me to a diverse range of projects, allowing me to adapt
-            swiftly to new technologies and frameworks. I am a strong advocate
-            for collaboration and effective communication, valuing the insights
-            and contributions of fellow developers and stakeholders alike.
+            My journey as a developer has exposed me to a diverse range of
+            projects, allowing me to adapt swiftly to new technologies and
+            frameworks. I am a strong advocate for collaboration and effective
+            communication, valuing the insights and contributions of fellow
+            developers and stakeholders alike.
             <br />
             <br />
             Outside of coding, I find inspiration in exploring emerging
@@ -43,12 +43,13 @@ function AboutMe() {
         transition={{ duration: 0.75, ease: [0.4, 0.3, 0.2, 1] }}
         exit={{ opacity: 1 }}
       >
-        <Image></Image>
+        <Image src="profilepic.jpg" alt="profile picture"></Image>
         <ImageFrame></ImageFrame>
       </Dark>
     </Body>
   );
 }
+
 
 const Body = styled.div`
   height: 100vh;
@@ -89,13 +90,15 @@ const Dark = styled(m.div)`
   background-color: var(--dark);
 `;
 
-const Image = styled.div`
+const Image = styled.img`
   position: relative;
   height: 55vh;
   min-width: 40vh;
   border-radius: 15px;
   background-color: #fff;
-`
+  filter: grayscale(40%);
+  z-index: 1;
+`;
 const ImageFrame = styled.div`
   position: absolute;
   height: 55vh;
@@ -104,7 +107,6 @@ const ImageFrame = styled.div`
   min-width: 40vh;
   border: 1px solid white;
   border-radius: 15px;
-
-`
+`;
 
 export default AboutMe;
