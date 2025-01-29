49de80e5e8a07c9db89c4f5accfb94a24d1ce34a
Emil Stjernlöf <emil.stjernlof@gmail.com>
2025-01-29 20:05:12 +0100

    Fixed Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/My-portfolio/', 
-  optimizeDeps: {
-    include: ['framer-motion'], 
-  },
+
   resolve: {
     alias: {
-      'framer-motion': resolve(__dirname, 'node_modules/framer-motion'),
     },
   },
+
+  optimizeDeps: {
+    include: ['framer-motion'], 
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+
   build: {
     rollupOptions: {
-      external: ['framer-motion'],
       output: {
         globals: {
-          'framer-motion': 'framerMotion'
-        }
-      }
+          'framer-motion': 'framerMotion',
+        },
+      },
     },
     commonjsOptions: {
       include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  },
-  optimizeDeps: {
-    include: ['framer-motion'],
-    esbuildOptions: {
-      target: 'esnext'
-    }
+      transformMixedEsModules: true,
+    },
   }
 });
