5e550b385c1f3653cd8b528b302e143b60811847
Emil Stjernlöf <emil.stjernlof@gmail.com>
2025-01-29 18:55:43 +0100

    Optimize Vite configuration for Framer Motion compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'framer-motion': path.resolve(__dirname, 'node_modules/framer-motion')
+      'framer-motion': 'framer-motion/dist/framer-motion'
     }
   },
   build: {
-    target: 'esnext', 
-    esbuild: {
-      target: 'esnext', 
+    commonjsOptions: {
+      include: [/framer-motion/]
     }
   }
 });
